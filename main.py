028037e92ee2df185dd95dd91af1878b4417d0ca
Nicole Edwards <114358549+nicoleedwards4@users.noreply.github.com>
2022-10-29 18:56:27 -0700

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.amazon.com")
+
